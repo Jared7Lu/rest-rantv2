02e5c7aef8bb3b4d26c672fa87fcb5020bee34e1
jalp21@icloud.com <jalp21@icloud.com>
2022-12-12 20:53:29 -0800

    part 5

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,31 @@
-const router = require('express').Router()
 
+const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+  
 //Get places
 router.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/rest1.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/rest2.jpg'
-  }]
-
   res.render('places/index', { places })
-
 })
 
-
-
+router.post('/', (req, res) => {
+    if (!req.body.pic) {
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
+  
   
   
 module.exports = router
