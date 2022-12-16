13ccfdb8b1156aa1459b33fec91488fe242a6207
jalp21@icloud.com <jalp21@icloud.com>
2022-12-16 11:53:07 -0800

    part 9

--- controllers/places.js
+++ controllers/places.js
@@ -1,102 +1,3 @@
-// const router = require('express').Router()
-// const places = require('../models/places.js')
-
-// //Get places
-// router.get('/', (req, res) => {
-//     res.render('places/index', {places})
-//   })
-
-// router.post('/', (req, res) => {
-//     if (!req.body.pic) {
-//       req.body.pic = 'http://placekitten.com/400/400'
-//     }
-//     if (!req.body.city) {
-//       req.body.city = 'Anytown'
-//     }
-//     if (!req.body.state) {
-//       req.body.state = 'USA'
-//     }
-//     places.push(req.body)
-//     res.redirect('./places')
-//   })
-
-// //get places new
-// router.get('/new', (req, res) => {
-//     res.render('places/new')
-//   })
-
-
-// //Show
-// router.get('/:id', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//       res.render('error404')
-//     }
-//     else if (!places[id]) {
-//       res.render('error404')
-//     }
-//     else {
-//         res.render('places/show', { place: places[id], id })
-//     }
-//   })
-
-// router.put('/:id', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//         res.render('error404')
-//     }
-//     else if (!places[id]) {
-//         res.render('error404')
-//     }
-//     else {
-//         // Dig into req.body and make sure data is valid
-//         if (!req.body.pic) {
-//             // Default image if one is not provided
-//             req.body.pic = 'http://placekitten.com/400/400'
-//         }
-//         if (!req.body.city) {
-//             req.body.city = 'Anytown'
-//         }
-//         if (!req.body.state) {
-//             req.body.state = 'USA'
-//         }  
-//         // Save the new data into places[id]
-//         places[id] = req.body
-//         res.redirect(`/places/${id}`)
-//     }
-//   })
-
-// //Delete
-// router.delete('/:id', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//       res.render('error404')
-//     }
-//     else if (!places[id]) {
-//       res.render('error404')
-//     }
-//     else {
-//       places.splice(id, 1)
-//       res.redirect('/places')
-//     }
-//   })
-
-//   //Edit
-// router.get('/:id/edit', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//         res.render('error404')
-//     }
-//     else if (!places[id]) {
-//         res.render('error404')
-//     }
-//     else {
-//       res.render('places/edit', { place: places[id], id })
-//     }
-// })
-  
-  
-
 const router = require('express').Router()
 const db = require('../models')
 
@@ -114,15 +15,26 @@ router.get('/', (req, res) => {
 
 //Post
 router.post('/', (req, res) => {
-    db.Place.create(req.body)
-    .then(() => {
-        res.redirect('/places')
-    })
-    .catch(err => {
-        console.log('err', err)
-        res.render('error404')
-    })
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
   })
+  .catch(err => {
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+      }
+      console.log('Validation error message', message)
+      res.render('places/new', { message })
+    }
+    else {
+      res.render('error404')
+  }
+  })
+})
+
   
 
 router.get('/new', (req, res) => {
