015d4330d903babbab98dc224e09b027c34fd083
jalp21@icloud.com <jalp21@icloud.com>
2022-12-16 17:19:33 -0800

    part 10

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -18,30 +18,29 @@ function new_form (data) {
           {message}
           <form method="POST" action="/places">
             <div className="form-group">
-            <label htmlFor="name">Place Name</label>
-            <input className="form-control" id="name" name="name" required />
+              <label htmlFor="name">Place Name</label>
+              <input className="form-control" id="name" name="name" required />
             </div>
             <div className="form-group">
-            <label htmlFor="pic">Place Picture</label>
-            <input className="form-control" id="pic" name="pic" />
+              <label htmlFor="pic">Place Picture</label>
+              <input className="form-control" id="pic" name="pic" />
             </div>
             <div className="form-group">
-            <label htmlFor="city">City</label>
-            <input className="form-control" id="city" name="city" />
+              <label htmlFor="city">City</label>
+              <input className="form-control" id="city" name="city" />
             </div>
             <div className="form-group">
-            <label htmlFor="state">State</label>
-            <input className="form-control" id="state" name="state" />
+              <label htmlFor="state">State</label>
+              <input className="form-control" id="state" name="state" />
             </div>
             <div className="form-group">
-            <label htmlFor="cuisines">Cuisines</label>
-            <input className="form-control" id="cuisines" name="cuisines" required />
+              <label htmlFor="cuisines">Cuisines</label>
+              <input className="form-control" id="cuisines" name="cuisines" required />
             </div>
             <div className="form-group">
-            <label for="founded">Founded Year</label>
-            <input type="number" className="form-control" id="founded" name="founded" value={new Date().getFullYear}/>
-            <input className="btn btn-primary" type="submit" value="Add Place" />
-            
+              <label for="founded">Founded Year</label>
+              <input type="number" className="form-control" id="founded" name="founded" value={new Date().getFullYear}/>
+              <input className="btn btn-primary" type="submit" value="Add Place" />
             </div>
           </form>
         </main>
